6ade8cf090dcf5b1fbc73397e9052e99584e4909
Gabriel Iuga <gabriel.iuga90@gmail.com>
2020-01-19 19:47:47 +0000

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `laravel-nova-mail-log` will be documented in this file
+
+## 1.0.0 - 2020-01-19
+
+- initial release
---
+++ src/MailLog.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Giuga\LaravelNovaMailLog;
+
+use App\Nova\Resource;
+use Giuga\LaravelNovaFieldIframe\Iframe;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+
+class MailLog extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \Giuga\LaravelMailLog\Models\MailLog::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'to', 'cc', 'bcc'
+    ];
+
+    public static function label()
+    {
+        return 'Mail Log';
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('To')->sortable(),
+            Text::make('Subject')->sortable(),
+            Text::make('Cc')->sortable(),
+            Text::make('Bcc')->sortable(),
+            DateTime::make('Created At')->sortable(),
+            Iframe::make('Message')->onlyOnDetail()
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
